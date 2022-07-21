2edecea5c8a638a2498606e2c048c977a62160e3
HP <HP@Michael>
2022-07-21 00:54:11 -0500

    Programación Orientada a Objetos_INT_2do "A" Versión1.2_Examen Día 21 / Julio / 2022

--- src/ec/edu/intsuperior/modelo/Clase_1.java
+++ src/ec/edu/intsuperior/modelo/Clase_1.java
@@ -2,4 +2,39 @@ package ec.edu.intsuperior.modelo;
 
 public class Clase_1 {
     
+    public static void MetodoTeoria1(){
+        System.out.println("La  recive Michael>"
+                + "de la Cueva .");
+    }
+    public static void MetodoTeorioa2(){
+    
+        System.out.println("La recive Pedro>Torres. ");
+        
+    }
+    public static void MetodoTeoria3(){
+        System.out.println("La recive Karla>Villamar");
+        
+    }
+    public static void MetodoTeoria4(){
+        System.out.println("La recive  Giordana>Simba");
+    }
+    public static void MetodoTeoria5(){
+        System.out.println("La recive Daniela>Torres");
+    }
+    public void MetodoinstanciaLengua1(){
+        System.out.println("La materia  de :Base de datos1");
+    }
+    public void MetodoinstanciaLengua2(){
+        System.out.println("La materia  de :Base de datos2");
+    }
+    public void MetodoinstanciaLengua3(){
+        System.out.println("La materia de  :Base de datos3");
+    }
+    public void MetodoinstanciaLengua4(){
+        System.out.println("La materia de :Base de datos4");
+    }
+    public void MetodoinstanciaLengua5(){
+        System.out.println("La materia de :Base de datos5");
+    }
 }
+  
--- src/ec/edu/intsuperior/vista/Aplication.java
+++ src/ec/edu/intsuperior/vista/Aplication.java
@@ -1,5 +1,44 @@
 package ec.edu.intsuperior.vista;
 
 public class Aplication {
+
+    public Aplication() {
+        
+        
+        
+    }
+
+    ClaseJava obj = new ClaseJava();
+
+    System.out.println ("Clase1 :Materias Base de datos Int ");
+
+    public ClaseJava getObj() {
+        return obj;
+    }
+    obj.MetodoinstanciaLengua1 ();
+
+    ClaseJava.MetodoTeoria1 ();
+
+    obj.MetodoinstanciaLengua2 ();
+
+    ClaseJava.MetodoTeorioa2 ();
+
+    obj.MetodoinstanciaLengua3 ();
+
+    ClaseJava.MetodoTeoria3 ();
+
+    obj.MetodoinstanciaLengua4 ();
+
+    ClaseJava.MetodoTeoria4 ();
+
+    obj.MetodoinstanciaLengua5 ();
+
+    ClaseJava.MetodoTeoria5 ();
+
+    System.out.println ("............"); 
+
+    
     
 }
+   
+
